809b51fae6d9495aff54aab66fe6ff3df423cd8d
VERO MENICHETTI <ingvmenichetti@gmail.com>
2022-11-12 17:59:10 -0300

    Se agrego la página de Error404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 import RutasPublicas from './router/RutasPublicas';
 import Nav from './componentes/Nav';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
