3e5ffb6827cd08f5a13756caa2ec6995cceb97b1
BeeSluka <valeriasluka@gmail.com>
2022-11-13 00:09:22 -0300

    Agrega Navbar

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 import { Button } from 'react-bootstrap';
 import FormLogin from '../componentes/FormLogin'
 import FormRegister from '../componentes/FormRegister'
-
+import Navbar from '../componentes/Nav'
 const Login = () => {
     const [form, setForm] = useState(true);
 
     return (
         <>
+
             {form ? <FormLogin />
 
                 : <FormRegister />
