9e52de4f89ec9398a112db0def738b4705e92a98
Esteban Orrego <orregoe@gmail.com>
2023-02-14 23:04:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const isLogged = JSON.parse(localStorage.getItem("userLogged"));
 
   return (
-      <className="App">
+      <div className="App">
         <BrowserRouter>
           {!isLogged ?
             <RutasPublicas />
@@ -15,7 +15,7 @@ function App() {
             <RutasPrivadas />
           }
         </BrowserRouter>
-      </>
+      <div/>
   );
 }
 
